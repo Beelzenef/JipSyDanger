150b284bf5430318949839d2edde69650d2d9642
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-06-05 23:26:34 +0200

    Primeras prácticas

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Cambiando un parrafo
+function miNombre()
+{
+	var nombre = window.prompt("¿Cual es tu nombre?");
+	document.getElementById("hora").innerHTML = nombre;
+}
+
+// Cambiando el titulo
+function nuevoTitulo()
+{
+	var tituloNuevo = window.prompt("¿Nuevo titulo?");
+	document.getElementById("tit").innerHTML = tituloNuevo;
+}
+
+// Tomando parametro de input
+function nombrePJ(name)
+{
+	document.getElementById("hora").innerHTML = name;
+}
