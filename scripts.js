5f5a4acf715b14a82667c8fd13282d71ae4c5c2a
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-06-20 17:50:25 +0200

    Pruebas con XML

--- scripts.js
+++ scripts.js
@@ -24,3 +24,26 @@ function nombrePJ(name)
 function CambiarColorBG() {
 	document.body.style.backgroundColor = "darkslateblue";
 }
+
+function displayCD(i) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            myFunction(xmlhttp, i);
+        }
+    };
+    xmlhttp.open("GET", "cd_catalog.xml", true);
+    xmlhttp.send();
+}
+
+function myFunction(xml, i) {
+    var xmlDoc = xml.responseXML; 
+    x = xmlDoc.getElementsByTagName("CD");
+    document.getElementById("showCD").innerHTML =
+    "Artist: " +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "<br>Title: " +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "<br>Year: " + 
+    x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+}
